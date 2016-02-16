96878a4bee406d630e695d303adf525c67569daa
Petermuturi <pmuturi65@gmail.com>
2016-02-16 10:46:14 +0300

    added user accounts

--- movin.js
+++ movin.js
@@ -1,21 +1,50 @@
+Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+     tasks: function(){
+      if(Session.get('finished')){
+        return Tasks.find({checked: {$ne: true}});  
+      } else{
+      return Tasks.find();
     }
+     },
+     finished: function(){
+      return Session.get('finished');
+     }
+  });
+// handle events on add button
+  Template.body.events({
+    'submit .routine': function(event){
+      var title = event.target.title.value;
+
+      Tasks.insert({
+         title: title,
+         createdAt: new Date()
+      });
+      event.target.title.value = "";
+      return false;
+    },
+     'change .hide':function(event){
+         Session.set('finished', event.target.checked);
+     }
+     
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.main.events({
+   'click .check':function(){
+    Tasks.update(this._id,{$set: {checked: !this.checked}});
+   },
+    'click .delete': function(){
+      Tasks.remove(this._id);
     }
+   
   });
+    Accounts.ui.config({
+     passwordSignupFields: "USERNAME_ONLY"
+   });
+   
 }
-
+   
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
